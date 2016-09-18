c4cd379c33875f61e5ff86cb7443a29a6a36ea75
benjioh5 <benjioh5@gmail.com>
2016-09-18 23:48:51 +0900

    add Orbit-Theme

---
+++ js/contents/career.js
@@ -0,0 +1,65 @@
+const career={
+  profile : "직장인으로서의 커리어는 아직 존재하지 않습니다. 하지만, 대학 생활에 관련된 여러 일들을 경험하였습니다.",
+  experiences : [{
+     jobTitle : "부회장",
+     during : "2016" ,
+     company : "Hanyang Univ. Cert ICEWALL",
+     detail :"ICEWALL은 한양대학교 CERT이자 동아리로써, 정보 보안을 다루고 있는 동아리입니다. 웹 해킹, 리버싱, 익스플로잇 등의 정보 보안과 관련된 것들을 세미나와 스터디를 통해 배우며, 주기적으로 교내 보안점검을 하고 있습니다.",
+     location :"서울 왕십리"
+  }],
+  project :{
+    description :"",
+    list : [{
+      title : "Best of the Best 4기 : Guardians of IoT@Home",
+      url : "https://www.kitribob.kr",
+      role : "취약점 분석 트랙",
+      during :"2015 - 2016",
+      detail :"홈 IoT 및 임베디드 시스템 취약점 분석 프로젝트.",
+      location :"서울 강남"
+    },{
+      title : "슈퍼 챌린지 해커톤 1회",
+      url : "http://www.hankyung.com/news/app/newsview.php?aid=2015020480241",
+      role : "임베디드 개발",
+      during :"2015",
+      detail :"pH 및 색상 기반 스마트 소변 검사 IoT 제품, Helpee를 개발하였으며, 이를 통해 중소기업청장상, UTC기술혁신상을 수상하였습니다.",
+      location :"Seoul, Republic of Korea"
+    },{
+      title : "강원대학교 아이디어팩토리 메이커톤",
+      url : "",
+      role : "임베디드 개발",
+      during :"2016.4.8 - 4.9",
+      detail :"문이 열렸을 경우 스피커 사운드를 꺼주는 IoT 제품 개발, 3등",
+      location :"강원도 춘천"
+    }]
+  },
+  opensource :{
+    description :"아직까지 경험이 존재하지 않습니다.",
+    list : []
+  },
+  skills : [{
+    skill : "Python",
+    level : "75%"
+  },{
+    skill : "C",
+    level :"65%"
+  },{
+    skill : "C++",
+    level :"35%"
+  },{
+    skill :"C#",
+    level :"45%"
+  },{
+    skill :"JAVA",
+    level :"55%"
+  },{
+    skill : "Erlang",
+    level :"45%"
+  },{
+    skill : "javascript",
+    level :"45%"
+  },{
+    skill : "x86_asm",
+    level :"55%"
+  }]
+}
+module.exports= career;
---
+++ js/contents/info.js
@@ -0,0 +1,21 @@
+const userinfo={
+  profile:{
+    name : "Benjamin Hyo-keun Oh",
+    tag : "Univ student",
+    mail : "benjioh5@gmail.com",
+    phone : "-----------------",
+    blog : "benjioh5.com/blog",
+    linkedin : "linkedin.com/in/a",
+    github : "github.com/benjioh5",
+    twitter : "@benjioh5",
+    imgUrl : "../assets/img/profile.jpg"
+  },
+  educations : [{
+     major : "BSc in Computer Science and Engineering",
+     school : "Hanyang University" ,
+     during : "2014 - Present"
+  }],
+  languages : [{lang:"Korean",fluency:"Native"},{lang:"English",fluency:"SoSo"},{lang:"Japanese",fluency:"SoSo"}],
+  interests :["Gaming","Startups","Writing"]
+}
+module.exports= userinfo;
